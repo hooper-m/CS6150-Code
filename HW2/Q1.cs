6d4e7ebc40cd93f05aeb0aa631a5b495c01906f1
Gramphics <hooperm.161@gmail.com>
2021-12-01 23:16:36 -0700

    add stuff

---
+++ HW2/Q1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HW2 {
+    class Q1 {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+            int trials = 1;
+            long overheadTime, totalTime;
+            double workTime, prev = 0;
+            Console.WriteLine("k\tTime\tRatio");
+
+            for (int k = 1; k <= 55; k++) {
+                Console.Write($"{k}\t");
+
+                sw.Start();
+                while (sw.ElapsedMilliseconds < 1000)
+                    ;
+
+                sw.Restart();
+                for (int i = 0; i < trials; i++) {
+                    Fibonacci(k);
+                }
+
+                totalTime = sw.ElapsedMilliseconds;
+
+                sw.Restart();
+                for (int i = 0; i < trials; i++)
+                    ;
+
+                overheadTime = sw.ElapsedMilliseconds;
+                workTime = ((totalTime - overheadTime) / ((double) trials)) / 1000;
+                Console.Write($"{workTime}\t");
+                Console.WriteLine($"{(prev == 0.0 ? 0 : workTime / prev)}");
+                prev = workTime;
+            }
+
+            Console.WriteLine("done");
+            Console.Read();
+        }
+
+        static long Fibonacci(long k) {
+            if (k == 0 || k == 1) {
+                return 1;
+            }
+            else {
+                return Fibonacci(k - 1) + Fibonacci(k - 2);
+            }
+        }
+    }
+}
---
+++ HW5/Q1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW5 {
+    class Q1 {
+
+        const int N = 10_000_000;
+        const double PERCENT_A = 0.55;
+        const int TRIALS = 100;
+        static readonly int[] SAMPLE_SIZES = new int[] { 50, 100, 200, 400 };
+
+        static void Main(string[] args) {
+            Q1a();
+        }
+
+
+        static void Q1a() {
+            Random rng = new Random();
+            char[] votes = new char[N];
+
+            for (int i = 0; i < (int) (N * PERCENT_A); i++) {
+                votes[i] = 'A';
+            }
+            for (int i = (int) (N * PERCENT_A); i < votes.Length; i++) {
+                votes[i] = 'B';
+            }
+
+            votes = votes.OrderBy(_ => rng.Next()).ToArray();
+
+            List<HashSet<int>> samples = new();
+
+            foreach (int sampleSize in SAMPLE_SIZES) {
+                for (int i = 0; i < TRIALS; i++) {
+                    HashSet<int> sampleIndices = new();
+                    while (sampleIndices.Count < sampleSize) {
+                        int index = rng.Next(N);
+
+                        while (sampleIndices.Contains(index)) {
+                            index = rng.Next(N);
+                        }
+
+                        sampleIndices.Add(index);
+                    }
+                    samples.Add(sampleIndices);
+                }
+            }
+
+            Dictionary<int, int> majorityCountsBySampleSize = SAMPLE_SIZES.ToDictionary(s => s, _ => 0);
+
+            foreach (HashSet<int> sample in samples) {
+                int As = sample.Select(s => votes[s])
+                               .Count(v => v == 'A');
+
+                if (As > sample.Count / 2) {
+                    majorityCountsBySampleSize[sample.Count]++;
+                }
+            }
+
+            foreach ((int sampleSize, int count) in majorityCountsBySampleSize) {
+                Console.WriteLine($"Sample size {sampleSize}: {count} ~ {(double) count / TRIALS}");
+            }
+        }
+    }
+}
